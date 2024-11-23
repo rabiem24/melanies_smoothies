83017cdc0d4d7af64f6c680c5cad2475a519794f
rabiem24 <61584985+rabiem24@users.noreply.github.com>
2024-11-23 11:38:37 +0800

    Update streamlit_app.py
    Show nutrition data of the fruits chosen only

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,13 @@ ingredients_list = st.multiselect(
 if ingredients_list:
 
     ingredients_string = ''
-    
+
     for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+    
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -47,7 +51,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-        
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
