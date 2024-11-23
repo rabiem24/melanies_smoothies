6df3eca63c4d24d38ef83cca5b7cd1338f97f11e
rabiem24 <61584985+rabiem24@users.noreply.github.com>
2024-11-23 11:02:56 +0800

    Update streamlit_app.py
    Changed back the streamlit.title to st.title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-streamlit.title("My Parents New Healthy Diner")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
     """Replace this example with your own code!
     **And if you're new to Streamlit,** check
