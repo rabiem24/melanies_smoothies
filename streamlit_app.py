7c8287cb436840f542fdbe143e8fac959ccb2954
rabiem24 <61584985+rabiem24@users.noreply.github.com>
2024-11-25 10:36:21 +0800

    Update streamlit_app.py
    Commented out the ingredients string

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     
-    st.write(ingredients_string)
+    # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order+"""')"""
